6d06c186c2459bcb0eeb22ea0b450db9576e93a5
Rurin Haliza <rurinhaliza@gmail.com>
2023-05-06 10:36:55 +0700

    Update 2023_04_22_164959_create_pengumuman_table.php

--- database/migrations/2023_04_22_164959_create_pengumuman_table.php
+++ database/migrations/2023_04_22_164959_create_pengumuman_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('pengumuman_judul');
             $table->text('pengumuman_deskripsi');
             $table->date('pengumuman_tanggal');
-            $table->enum('pengumuman_status', ['publish', 'draft'])->default('draft');
+            $table->enum('pengumuman_status', ['aktif', 'tidak_aktif'])->default('tidak_aktif');
             $table->string('pengumuman_gambar');
         });
     }
